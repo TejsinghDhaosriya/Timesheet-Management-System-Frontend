d01f8b00f837ec5b28415687521aa3772b32f2ca
vignesh <ssaivignesh7@gmail.com>
2023-01-24 09:10:43 +0530

    side bar

--- timesheet/src/components/Navbar.tsx
+++ timesheet/src/components/Navbar.tsx
@@ -1,77 +1,22 @@
 import {
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
   Button,
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Drawer,
 } from "@mui/material";
-import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import EditIcon from '@mui/icons-material/Edit';
+import SideBar from "./SideBar";
 
 
 
 function Navbar() {
-  const [state, setState] = useState(false);
-  const toggleDrawer =(open: boolean) => () => {
-      setState(open);
-    };
-
-  const sidebar = () => (
-    <Box
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <EditIcon />
-            </ListItemIcon>
-            <ListItemText primary="projects" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <CalendarMonthIcon />
-            </ListItemIcon>
-            <ListItemText primary="Calendar" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
-  );
-
   return (
-    <AppBar position="static">
+    <AppBar >
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-          onClick={toggleDrawer(true)}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Drawer open={state} onClose={toggleDrawer(false)}>
-          {sidebar()}
-        </Drawer>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <SideBar/>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
           Time Sheet Management
         </Typography>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit">User</Button>
       </Toolbar>
     </AppBar>
   );
