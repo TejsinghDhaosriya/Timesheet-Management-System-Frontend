a9c3bd96231cee181c5cf0841f67b63090c37da1
Yash Jain <jainyash566@gmail.com>
2023-01-30 19:32:12 +0530

    merged code with dev one

--- timesheet/src/modules/timesheet-page/helper.ts
+++
@@ -1,5 +0,0 @@
-export const formSubmittedStatusHelper = (selectedDateArray:any,dateSelected:string)=>{
-    return selectedDateArray.find((dateSelect:any)=>{
-        return (dateSelect.timeSheetSubmitted===true && dateSelect.date===dateSelected);
-    });
-}
--- timesheet/src/sagas/index.tsx
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchUsersAsync } from "../modules/projects-page/saga/projectSaga";
-
-
-export function* rootSaga() {
-    yield all([
-        watchUsersAsync()
-    ])
-}
